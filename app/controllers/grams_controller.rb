135329ab660107a53642c80390e02618daf47c61
Leonardo Guzman <lguzman@me.com>
2016-09-26 20:11:50 +0000

    started css styling

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -54,8 +54,6 @@ end
     params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render text: "#{status.to_s.titleize}", status: status
-  end
+
 
 end
